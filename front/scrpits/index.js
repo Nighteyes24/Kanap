b3cf74a97ea1bcd7c2180051974034902ced3fe3
Nighteyes24 <cornelia.cros@gmail.com>
2022-08-31 12:33:17 +0200

    mentorat index

--- front/scrpits/index.js
+++ front/scrpits/index.js
@@ -1,19 +1,16 @@
 let couchData = [];
 
 //* fonction pour récupérer les data de l'API
-const fetchCouch = async () => {
-  await fetch("http://localhost:3000/api/products")
-    .then((res) => res.json())
-    .then((Promise) => {
-      couchData = Promise;
-      console.log(couchData);
-    });
-};
+async function fetchCouch() {
+  const res = await fetch("http://localhost:3000/api/products");
+  const data = await res.json();
+  return data;
+}
 
 //* fonction pour afficher les data
 
 const couchDisplay = async () => {
-  await fetchCouch();
+  couchData = await fetchCouch();
 
   document.getElementById("items").innerHTML = couchData
     .map(
