cd68de8566c132a945de3f922b926fd850af1398
Nighteyes24 <cornelia.cros@gmail.com>
2022-08-31 10:13:46 +0200

    page index

--- front/scrpits/index.js
+++ front/scrpits/index.js
@@ -1,2 +1,34 @@
+let couchData = [];
 
+//* fonction pour récupérer les data de l'API
+const fetchCouch = async () => {
+  await fetch("http://localhost:3000/api/products")
+    .then((res) => res.json())
+    .then((Promise) => {
+      couchData = Promise;
+      console.log(couchData);
+    });
+};
 
+//* fonction pour afficher les data
+
+const couchDisplay = async () => {
+  await fetchCouch();
+
+  document.getElementById("items").innerHTML = couchData
+    .map(
+      (couch) => `
+  <a href="./product.html?id=${couch._id}">
+            <article>
+                <img 
+                    src="${couch.imageUrl}" 
+                    alt="${couch.altTxt}">
+                <h3 class="productName">${couch.name}</h3><p class="productDescription">${couch.description}</p>
+            </article>
+                </a>
+  `
+    )
+    .join(" ");
+};
+
+couchDisplay();
