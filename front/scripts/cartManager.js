54c6f10f8005631f35b8775d3a125d354350ee7f
Nighteyes24 <cornelia.cros@gmail.com>
2022-10-19 10:34:03 +0200

    modif cart manager + ajout confirmation

--- front/scripts/cartManager.js
+++ front/scripts/cartManager.js
@@ -7,17 +7,23 @@ export function setCart(cart) {
 
 export function addToCart(product) {
   /**verifier que ds cart il y a 1 prod qui a id et color si oui on ajoute la quant au prod sinon on ajoute le produit au cart */
-  var cart = getCart();
-  if (typeof product == "undefined") {
-    cart.push(product);
-  } else typeof product <= 1;
-  {
-    Array.some(cart);
-  }
+  const cart = getCart();
+  const item = cart.find(
+    (item) => product.color + product.id == item.color + item.id
+  );
+
+  if (item) item.quantity += product.quantity;
+  else cart.push((item = product));
+
+  if (item.quantity > 100) item.quantity = 100;
+  setCart(cart);
+
+  if (item) item.color += product.color;
+  else cart.push((item = product));
 }
-
-setCart(cart);
-
+/**
+ * @return {Array}
+ *  structure du panier même si vide*/
 export function getCart() {
   return JSON.parse(localStorage.getItem("myCart")) || [];
 }
